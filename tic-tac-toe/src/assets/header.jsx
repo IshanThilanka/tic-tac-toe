c66ab7491f57181776204bfb585bdb529c7f020a
IshanThilanka <ishanthilanka98@gmail.com>
2023-10-30 00:19:47 +0530

    First Commit

---
+++ tic-tac-toe/src/assets/header.jsx
@@ -0,0 +1,11 @@
+ 
+ import "./header.css"
+ 
+ 
+ export function Header(){
+            return(
+                <header id="header">
+                    Tic-Tac-Toe
+                </header>
+            )
+}
